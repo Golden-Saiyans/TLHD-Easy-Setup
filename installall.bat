75710fe7cfc262d3e2bc54ef2d8f14231221e7f8
FloweyGaming <86134968+FloweyGaming@users.noreply.github.com>
2024-08-17 19:46:16 +0200

    Update installall.bat
    😭

--- installall.bat
+++ installall.bat
@@ -1,39 +1,45 @@
 @echo off
 color 0A
-echo I will install the mod !
+echo Welcome to the Tomodachi Life HD Online Installer !
 echo --------------------------------
-echo You will need 1 Gb of storage.
+echo You will need at least 1 GB of free space.
 timeout /t 5 /nobreak > NUL
 cls
 color 01
-echo I'm installing the mod !
+echo Downloading...
 
 curl.exe -LO https://github.com/FIREXDF/TLHD/archive/refs/heads/main.zip
 cls
 color 0E
-echo it's succesfully installed !
+echo Done !
 timeout /t 2 /nobreak > NUL
 cls
-echo Now let's extract the file !
+echo Extracting...
 tar -xf main.zip
 cls
-echo And for the last let's delete all the unnecessary file
+echo Cleaning up...
 timeout /t 5 /nobreak > NUL
 rm main.zip
 rm TLHD-main/README.md
 cls
 color 02
-echo We are done ! Thanks for installing our mod ! Don't forget to join our discord server ! (https://dsc.gg/tomodachilifehd)
+echo Installation Successful ! Thanks for installing our mod ! Don't forget to join our discord server ! (https://dsc.gg/tomodachilifehd)
 echo ---------------------------------------------
-echo Thanks to :
+echo Mod Made by: 
+echo FIREXDF and FloweyGaming from The Golden Saiyans Team
+
+echo Contributors:
 echo SighKick
 echo EliMCGamerGuy
 echo angelo/apcompsci
-echo AndrewTheModHunter for giving 30 points !
-echo tom for giving 10 points !
-echo Sea3Plays for giving 100 points !
-echo uhoaky for giving 10 points !
-echo akitauma for giving 10 points
+
+echo Special Thanks:
+echo AndrewTheModHunter for giving 30 points on GameBanana
+echo tom for giving 10 points on GameBanana
+echo Sea3Plays for giving 100 points on GameBanana
+echo uhoaky for giving 10 points on GameBanana
+echo akitauma for giving 10 points on GameBanana
+echo You for installing our mod
 echo ---------------------------------------------
 timeout /t 20
 rm installall.bat
