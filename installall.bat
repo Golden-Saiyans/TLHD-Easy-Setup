cb4f60db5b73da0ce00fcac1636aa0068da9f1b9
FloweyGaming <86134968+FloweyGaming@users.noreply.github.com>
2024-08-19 08:10:40 +0200

    Update installall.bat

--- installall.bat
+++ installall.bat
@@ -1,12 +1,12 @@
 @echo off
-REM Test if tar are install
+REM Test if tar is installed
 tar --version >nul 2>&1
 
 REM Verify the error code
 if %errorlevel% neq 0 (
 	echo You need to install tar.
 	echo.
-	echo Redirect to tar download page in 5 seconds...
+	echo Redirecting to tar download page in 5 seconds...
 	echo Don't forget to put tar into your Path !
 	timeout /t 5 /nobreak >NUL
 	start "" "https://sourceforge.net/projects/gnuwin32/files/tar/1.13-1/tar-1.13-1-bin.exe/download?use_mirror=altushost-swe&download="
